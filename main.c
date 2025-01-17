13d602203bc9e2ee874f004831cc8e14a7a6ae2f
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2025-01-17 13:48:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-﻿#include "funciones.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
 
 int main() {
     Factura facturas[100];
     int numFacturas = 0;
-
     int opcion;
+
+    cargarFacturasDesdeArchivo(facturas, &numFacturas, "facturas.txt");
+
     do {
         printf("\n--- Menú ---\n");
         printf("1. Agregar factura\n");
@@ -27,9 +31,11 @@ int main() {
                 break;
             case 4:
                 guardarFacturasEnArchivo(facturas, numFacturas, "facturas.txt");
+                guardarCedulasEnArchivo(facturas, numFacturas, "cedulas.txt");
+                guardarProductosEnArchivo(facturas, numFacturas, "productos.txt");
                 break;
             case 5:
-                printf("Saliendo...\n");
+                printf("¡Adiós!\n");
                 break;
             default:
                 printf("Opción no válida.\n");
